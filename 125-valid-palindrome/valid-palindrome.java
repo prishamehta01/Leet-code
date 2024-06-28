e6be09ccd2700331b6c10c271743537d5944f2a8
Prisha Mehta <143733402+prishamehta01@users.noreply.github.com>
2024-06-28 22:34:10 +0530

    Time: 14 ms (34.91%) | Memory: 45.1 MB (20.08%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -2,13 +2,24 @@ class Solution {
     public boolean isPalindrome(String s) {
         s=s.replaceAll("[^a-zA-Z0-9]", "");
         s=s.toLowerCase();
-        for(int i=0;i<s.length()/2;i++)
+        /*for(int i=0;i<s.length()/2;i++)
         {
             if(s.charAt(i)!=s.charAt(s.length()-i-1))
             {
                 return false;
             }
         }
-        return true;
+        return true;*/
+        StringBuilder rev=new StringBuilder(s);
+        rev.reverse();
+        String a=rev.toString();
+        if(s.equals(a))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
