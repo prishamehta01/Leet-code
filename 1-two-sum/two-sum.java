3f438b996fbccc26117ec1a38a1ba62b7ec848de
Prisha Mehta <143733402+prishamehta01@users.noreply.github.com>
2024-07-21 15:58:37 +0530

    Time: 45 ms (30.09%) | Memory: 44.7 MB (52.97%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for(int i=0;i<nums.length-1;i++)
+        for(int i=0;i<nums.length;i++)
+        {
             for(int j=i+1;j<nums.length;j++)
             {
                 if(nums[i]+nums[j]==target)
@@ -8,6 +9,7 @@ class Solution {
                     return new int[]{i,j};
                 }
             }
-            return new int[]{};
+        }
+        return new int[]{};
     }
 }
