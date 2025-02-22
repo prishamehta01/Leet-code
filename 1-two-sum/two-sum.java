1e72dc14d7c62728bc360c7e6cf35daa422090e8
Prisha Mehta <143733402+prishamehta01@users.noreply.github.com>
2025-02-22 15:28:07 +0530

    Time: 4 ms (60.44%) | Memory: 45.2 MB (17.53%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,27 +1,30 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[][] pairs = new int[nums.length][2];
-        for (int i = 0; i < nums.length; i++) {
-            pairs[i][0] = nums[i];  // value
-            pairs[i][1] = i;        // original index
-        }
-        
-        // Sort based on values
-        Arrays.sort(pairs, (a, b) -> a[0] - b[0]);
-        
-        // Two pointer approach
-        int left = 0, right = nums.length - 1;
-        while (left < right) {
-            int sum = pairs[left][0] + pairs[right][0];
-            if (sum == target) {
-                return new int[]{pairs[left][1], pairs[right][1]};
-            } else if (sum < target) {
+        int[] sorted = nums.clone();
+        Arrays.sort(sorted);
+        int left=0,right=nums.length-1;
+        while(left<right){
+            int sum = sorted[left]+sorted[right];
+            if(sum==target){
+                int index1=-1,index2=-1;
+                for(int i=0;i<nums.length;i++){
+                    if(index1==-1 && nums[i]==sorted[left]){
+                        index1=i;
+                    }
+                    else if(nums[i]==sorted[right]){
+                        index2 = i;
+                    }
+                }
+                return new int[]{index1,index2};
+            }
+            else if(sum<target){
                 left++;
-            } else {
+            }
+            else{
                 right--;
             }
+
         }
-        
         return new int[]{};
     }
 }
