042a70dd06b0ea8f28604a380c21673013d0a9e7
Prisha Mehta <143733402+prishamehta01@users.noreply.github.com>
2025-02-25 23:10:48 +0530

    Time: 9 ms (26.68%) | Memory: 57.6 MB (83.30%) - LeetSync

--- 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.java
+++ 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.java
@@ -1,23 +1,23 @@
 class Solution {
     public int numOfSubarrays(int[] arr) {
         final int MOD = 1000000007;
-    long count = 0;      // Store result safely
-    long sum = 0;        // Use long for prefix sum
-    long oddCount = 0;   
-    long evenCount = 1;   // Start with 1 for subarrays starting at index 0
-    
-    for (int num : arr) {
-        sum += num;
+        long count = 0;     
+        long sum = 0;       
+        long oddCount = 0;   
+        long evenCount = 1;  
         
-        if (sum % 2 != 0) { // odd prefix sum
-            count = (count + evenCount) % MOD; // count subarrays with previous even prefix sums
-            oddCount++;
-        } else { // even prefix sum
-            count = (count + oddCount) % MOD; // count subarrays with previous odd prefix sums
-            evenCount++;
+        for (int num : arr) {
+            sum += num;
+            if (sum % 2 != 0) { 
+                count = (count + evenCount) % MOD;       
+                oddCount++;
+            } 
+            else {
+                count = (count + oddCount) % MOD; 
+                evenCount++;
+            }
         }
-    }
-    
-    return (int) (count % MOD);
+        
+        return (int) (count % MOD);
     }
 }
