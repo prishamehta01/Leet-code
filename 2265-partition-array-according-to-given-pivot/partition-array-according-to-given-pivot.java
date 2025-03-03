5aff34d2b083cca6590af5a9dcc654c0abc76774
Prisha Mehta <143733402+prishamehta01@users.noreply.github.com>
2025-03-03 23:38:48 +0530

    Time: 5 ms (85.39%) | Memory: 68.1 MB (44.51%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -1,23 +1,16 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        ArrayList<Integer> less = new ArrayList<>();
-        ArrayList<Integer> eq = new ArrayList<>();
-        ArrayList<Integer> more = new ArrayList<>();
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]<pivot) less.add(nums[i]);
-            else if(nums[i]==pivot) eq.add(nums[i]);
-            else more.add(nums[i]);
-        }
+        int[] res = new int[nums.length];
         int index = 0;
-        for(int x:less){
-            nums[index++]=x;
+        for(int x:nums){
+            if(x<pivot) res[index++]=x;
         }
-        for(int y:eq){
-            nums[index++] = y;
+        for(int y:nums){
+            if(y==pivot) res[index++] = y;
         }
-        for(int z:more){
-            nums[index++] = z;
+        for(int z:nums){
+            if(z>pivot) res[index++] = z;
         }
-        return nums;
+        return res;
     }
 }
