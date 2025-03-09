c12ab0823be8fb9ca00476c150897cb18d0ce9f1
Prisha Mehta <143733402+prishamehta01@users.noreply.github.com>
2025-03-09 11:58:01 +0530

    Time: 4 ms (68.16%) | Memory: 56.7 MB (88.55%) - LeetSync

--- 3483-alternating-groups-ii/alternating-groups-ii.java
+++ 3483-alternating-groups-ii/alternating-groups-ii.java
@@ -1,19 +1,18 @@
 class Solution {
     public int numberOfAlternatingGroups(int[] colors, int k) {
-        int n = colors.length;
-        int count = 0;
-        int left = 0;
-        
-        for (int right = 0; right < n + k - 1; right++) {
-            if (right > 0 && colors[right % n] == colors[(right - 1) % n]) {
-                left = right;  
+        int[] nums=new int[colors.length+k-1];
+        int i=0;
+        for(int e:colors)nums[i++]=e;
+        for(int j=0;j<k-1;j++) nums[i++]=colors[j];
+        int left=0,right=0,count=0;
+        while(right<nums.length){
+            if(right!=0&&nums[right]==nums[right-1])left=right;
+            if(right-left+1>=k){
+                 count++;
+
             }
-            
-            if (right - left + 1 >= k) {
-                count++;  
-            }
+            right++;
         }
-        
-        return count;
+        return  count;
     }
 }
