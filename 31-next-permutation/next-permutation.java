9f22cb550817abdb9def135d7837ba6aebb61113
Prisha Mehta <143733402+prishamehta01@users.noreply.github.com>
2025-03-14 00:29:00 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (21.31%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,9 +1,9 @@
 class Solution {
-    public void rev(int[] nums,int left,int right){
+    public void rev(int[] a, int left, int right){
         while(left<right){
-            int temp = nums[left];
-            nums[left]= nums[right];
-            nums[right]= temp;
+            int temp = a[left];
+            a[left] = a[right];
+            a[right]=temp;
             left++;
             right--;
         }
@@ -12,7 +12,7 @@ class Solution {
         int index = -1;
         for(int i=nums.length-2;i>=0;i--){
             if(nums[i]<nums[i+1]){
-                index=i;
+                index = i;
                 break;
             }
         }
@@ -20,11 +20,12 @@ class Solution {
             rev(nums,0,nums.length-1);
             return;
         }
-        for(int i=nums.length-1;i>index;i--){
-            if(nums[i]>nums[index]){
-                int temp = nums[i];
-                nums[i]=nums[index];
-                nums[index]=temp;
+
+        for(int j=nums.length-1;j>index;j--){
+            if(nums[j]>nums[index]){
+                int temp = nums[j];
+                nums[j] = nums[index];
+                nums[index] = temp;
                 break;
             }
         }
