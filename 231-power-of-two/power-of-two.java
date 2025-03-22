dcad40ff03db41c6c5ea94e00052e39f9b49f447
Prisha Mehta <143733402+prishamehta01@users.noreply.github.com>
2025-03-22 12:20:18 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (47.91%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,7 +1,9 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
         if(n<=0) return false;
-        double res = Math.log(n)/Math.log(2);
-        return Math.abs(res - Math.round(res)) < 1e-10;
+        while(n%2==0){
+            n/=2;
+        }
+        return n==1;
     }
 }
