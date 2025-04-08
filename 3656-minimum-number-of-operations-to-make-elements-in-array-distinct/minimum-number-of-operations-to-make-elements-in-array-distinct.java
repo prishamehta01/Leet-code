464ad1a5c10278db352ff186c25b29567aba33b0
Prisha Mehta <143733402+prishamehta01@users.noreply.github.com>
2025-04-08 15:03:57 +0530

    Time: 1 ms (98.39%) | Memory: 44.8 MB (16.29%) - LeetSync

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.java
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.java
@@ -3,7 +3,7 @@ class Solution {
         boolean[] present = new boolean[128];
         for(int i=nums.length-1;i>=0;i--){
             if(present[nums[i]]){
-                return (i+3)/3;
+                return i/3+1;
             }
             present[nums[i]] = true;
         }
