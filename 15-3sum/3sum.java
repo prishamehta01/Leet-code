f404de553be8c7cc0697cb5b52428f3f01a9a5d9
Prisha Mehta <143733402+prishamehta01@users.noreply.github.com>
2025-06-11 23:28:08 +0530

    Time: 32 ms (43.75%) | Memory: 51.2 MB (92.19%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,28 +1,34 @@
+import java.util.*;
+
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        int n = nums.length;
-        Arrays.sort(nums);
-        for(int i=0;i<n-2;i++){
-            if(i>0 && nums[i]==nums[i-1]) continue;
-            int left = i+1;
-            int right = n-1;
+        Arrays.sort(nums);  // Step 1
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            // Skip duplicate 'i'
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int left = i + 1;
+            int right = nums.length - 1;
             int target = -nums[i];
-            while(left<right){
-                if(nums[left]+nums[right]==target){
-                    res.add(Arrays.asList(nums[i],nums[left],nums[right]));
 
-                    while(left<right && nums[left]==nums[left+1]) left++;
-                    while(left<right && nums[right]==nums[right-1]) right--;
+            while (left < right) {
+                int sum = nums[left] + nums[right];
+
+                if (sum == target) {
+                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    // Skip duplicate left/right
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
 
                     left++;
                     right--;
-                }
-                else if(nums[left]+nums[right]>target){
-                    right--;
-                }
-                else{
+                } else if (sum < target) {
                     left++;
+                } else {
+                    right--;
                 }
             }
         }
