6b593f03d6fc6605cc019982b9cc7ec5b2ee5396
Prisha Mehta <143733402+prishamehta01@users.noreply.github.com>
2025-06-12 11:10:36 +0530

    Time: 5 ms (57.64%) | Memory: 42.9 MB (88.67%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()) return false;
+        int[] freq_s = new int[26];
+        int[] freq_t = new int[26];
+        for(int i=0;i<s.length();i++){
+            freq_s[s.charAt(i)-'a']++;
+            freq_t[t.charAt(i)-'a']++;
+        }
+        for(int i=0;i<26;i++){
+            if(freq_s[i]!=freq_t[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
