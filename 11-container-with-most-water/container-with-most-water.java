5955294234707624e3f3e99d9de60b595e156f35
Prisha Mehta <143733402+prishamehta01@users.noreply.github.com>
2025-06-12 08:22:05 +0530

    Time: 5 ms (75.15%) | Memory: 58 MB (54.85%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -4,16 +4,11 @@ class Solution {
         int right = height.length-1;
         int maxCon = Integer.MIN_VALUE;
         while(left<right){
+            maxCon = Math.max(maxCon,(right-left)*(Math.min(height[left],height[right])));
             if(height[left]<height[right]){
-                int width = right-left;
-                int heigh = Math.min(height[left],height[right]);
-                maxCon = Math.max(maxCon,width*heigh);
                 left++;
             }
             else{
-                int width = right-left;
-                int heigh = Math.min(height[left],height[right]);
-                maxCon = Math.max(maxCon,width*heigh);
                 right--;
             }
         }
