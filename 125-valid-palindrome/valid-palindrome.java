bd52dcdd3dc77f15baee2973bbc567b53019b851
Prisha Mehta <143733402+prishamehta01@users.noreply.github.com>
2025-07-16 12:22:54 +0530

    Time: 3 ms (72.96%) | Memory: 43.5 MB (58.80%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,15 +1,16 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        int left = 0;
-        int right = s.length()-1;
-        while(left<right){
-            while(left<right && !Character.isLetterOrDigit(s.charAt(left))) left++;
-            while(left<right && !Character.isLetterOrDigit(s.charAt(right))) right--;
-            if(Character.toLowerCase(s.charAt(left))!=Character.toLowerCase(s.charAt(right))){
+        s = s.toLowerCase();
+        int l = 0;
+        int r = s.length()-1;
+        while(l<r){
+            while(!Character.isLetterOrDigit(s.charAt(l)) && l<r) l++;
+            while(!Character.isLetterOrDigit(s.charAt(r)) && l<r) r--;
+            if(s.charAt(l)!=s.charAt(r)){
                 return false;
             }
-            left++;
-            right--;
+            l++;
+            r--;
         }
         return true;
     }
