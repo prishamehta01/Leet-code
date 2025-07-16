ed43efc84a57e032830007cc1c7bf3f552ef111f
Prisha Mehta <143733402+prishamehta01@users.noreply.github.com>
2025-07-16 10:36:57 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (89.48%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,6 +1,5 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        //Transpose
         for(int i=0;i<matrix.length;i++){
             for(int j=i;j<matrix[0].length;j++){
                 int temp = matrix[i][j];
@@ -8,13 +7,11 @@ class Solution {
                 matrix[j][i] = temp;
             }
         }
-
-        //Reverse each row
         for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix[0].length/2;j++){
                 int temp = matrix[i][j];
-                matrix[i][j] = matrix[i][matrix.length-j-1];
-                matrix[i][matrix.length-j-1] = temp;
+                matrix[i][j] = matrix[i][matrix.length-1-j];
+                matrix[i][matrix.length-1-j] = temp;
             }
         }
     }
