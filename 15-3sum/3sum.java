1108b0434ff788997f9b2b7a229ab3bd10f9a818
Prisha Mehta <143733402+prishamehta01@users.noreply.github.com>
2025-07-16 09:59:23 +0530

    Time: 31 ms (56.78%) | Memory: 51.6 MB (58.12%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -5,16 +5,14 @@ class Solution {
         Arrays.sort(nums);
         for(int i=0;i<n-2;i++){
             if(i>0 && nums[i]==nums[i-1]) continue;
+            int target = -nums[i];
             int left = i+1;
             int right = n-1;
-            int target = -nums[i];
             while(left<right){
                 if(nums[left]+nums[right]==target){
                     res.add(Arrays.asList(nums[i],nums[left],nums[right]));
-
                     while(left<right && nums[left]==nums[left+1]) left++;
                     while(left<right && nums[right]==nums[right-1]) right--;
-
                     left++;
                     right--;
                 }
