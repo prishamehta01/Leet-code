7d66670fad4d932b5c65d258aa5b6ef9f565ffe3
Prisha Mehta <143733402+prishamehta01@users.noreply.github.com>
2025-07-23 23:33:10 +0530

    Time: 2 ms (5.54%) | Memory: 43.9 MB (6.61%) - LeetSync

--- 112-path-sum/path-sum.java
+++ 112-path-sum/path-sum.java
@@ -1,25 +1,38 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    public boolean hasPathSum(TreeNode root, int targetSum) {
-        if(root==null) return false;
-        targetSum = targetSum-root.val;
-        if(root.left==null && root.right==null){
-            return targetSum == 0;
+    class NodeSum {
+        TreeNode node;
+        int sum;
+
+        NodeSum(TreeNode node, int sum) {
+            this.node = node;
+            this.sum = sum;
         }
-        return hasPathSum(root.left,targetSum)||hasPathSum(root.right,targetSum);
+    }
+
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) return false;
+
+        Stack<NodeSum> stack = new Stack<>();
+        stack.push(new NodeSum(root, root.val));
+
+        while (!stack.isEmpty()) {
+            NodeSum current = stack.pop();
+            TreeNode node = current.node;
+            int sum = current.sum;
+
+            if (node.left == null && node.right == null && sum == targetSum) {
+                return true;
+            }
+
+            if (node.right != null) {
+                stack.push(new NodeSum(node.right, sum + node.right.val));
+            }
+
+            if (node.left != null) {
+                stack.push(new NodeSum(node.left, sum + node.left.val));
+            }
+        }
+
+        return false;
     }
 }
