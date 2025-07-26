dab13127a5e328286acaaa9bf6548b8eec3bb43d
Prisha Mehta <143733402+prishamehta01@users.noreply.github.com>
2025-07-26 16:18:31 +0530

    Time: 3 ms (17.56%) | Memory: 42 MB (20.28%) - LeetSync

--- 274-h-index/h-index.java
+++ 274-h-index/h-index.java
@@ -1,20 +1,22 @@
 class Solution {
     public int hIndex(int[] citations) {
+        Arrays.sort(citations);
         int n = citations.length;
-        int maxH = 0;
+        int left = 0, right = n - 1;
+        int hIndex = 0;
 
-        for (int h = 0; h <= n; h++) {
-            int count = 0;
-            for (int c : citations) {
-                if (c >= h) {
-                    count++;
-                }
-            }
-            if (count >= h) {
-                maxH = h;  // we try to maximize h
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int h = n - mid; // number of papers with at least citations[mid] citations
+
+            if (citations[mid] >= h) {
+                hIndex = h; // candidate for h-index
+                right = mid - 1; // try to find a better (higher) h to the left
+            } else {
+                left = mid + 1; // move right
             }
         }
 
-        return maxH;
+        return hIndex;
     }
 }
