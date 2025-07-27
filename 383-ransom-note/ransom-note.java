f9e24caa96a3bea871926f1fbf47d6cde12c34bf
Prisha Mehta <143733402+prishamehta01@users.noreply.github.com>
2025-07-27 23:13:01 +0530

    Time: 3 ms (65.02%) | Memory: 44.6 MB (92.01%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -1,15 +1,15 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        if (ransomNote.length() > magazine.length()) return false;
-        int[] alphabets_counter = new int[26];
-        
-        for (char c : magazine.toCharArray())
-            alphabets_counter[c-'a']++;
+        StringBuilder sb = new StringBuilder(magazine);
 
-        for (char c : ransomNote.toCharArray()){
-            if (alphabets_counter[c-'a'] == 0) return false;
-            alphabets_counter[c-'a']--;
+        for (char c : ransomNote.toCharArray()) {
+            int index = sb.indexOf(String.valueOf(c));
+            if (index == -1) {
+                return false; // character not found in magazine
+            }
+            sb.deleteCharAt(index); // use the character only once
         }
+
         return true;
     }
 }
