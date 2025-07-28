0e01327ee1fe20818fed3b90343d35050a462260
Prisha Mehta <143733402+prishamehta01@users.noreply.github.com>
2025-07-29 00:17:13 +0530

    Time: 3 ms (65.08%) | Memory: 44.8 MB (65.56%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -1,15 +1,13 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
         StringBuilder sb = new StringBuilder(magazine);
-
-        for (char c : ransomNote.toCharArray()) {
+        for(char c:ransomNote.toCharArray()){
             int index = sb.indexOf(String.valueOf(c));
-            if (index == -1) {
-                return false; // character not found in magazine
+            if(index==-1){
+                return false;
             }
-            sb.deleteCharAt(index); // use the character only once
+            sb.deleteCharAt(index);
         }
-
         return true;
     }
 }
