a3c6bf87a53bfc71fd1b381cc0c917beeee100f2
Prisha Mehta <143733402+prishamehta01@users.noreply.github.com>
2025-08-19 23:43:07 +0530

    Time: 50 ms (52.59%) | Memory: 55.1 MB (61.85%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.java
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -1,17 +1,22 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-        int l=0,r=0,maxLen=0;
-        HashMap<Integer,Integer> map = new HashMap<>();
-        while(r<fruits.length){
-            map.put(fruits[r],map.getOrDefault(fruits[r],0)+1);
-            if(map.size()>2){
-                map.put(fruits[l],map.get(fruits[l])-1);
-                if(map.get(fruits[l])==0)   map.remove(fruits[l]);
+        int l = 0, maxLen = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int r = 0; r < fruits.length; r++) {
+            map.put(fruits[r], map.getOrDefault(fruits[r], 0) + 1);
+
+            while (map.size() > 2) {
+                map.put(fruits[l], map.get(fruits[l]) - 1);
+                if (map.get(fruits[l]) == 0) {
+                    map.remove(fruits[l]);
+                }
                 l++;
             }
-            maxLen = Math.max(maxLen,r-l+1);
-            r++;
+
+            maxLen = Math.max(maxLen, r - l + 1);
         }
+
         return maxLen;
     }
 }
