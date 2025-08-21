c9e25427dbc4cc7f7381494e0789b6066b3aa328
Prisha Mehta <143733402+prishamehta01@users.noreply.github.com>
2025-08-21 23:17:41 +0530

    Time: 12 ms (73.28%) | Memory: 44.8 MB (49.54%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -1,20 +1,13 @@
 class Solution {
     public int numberOfSubstrings(String s) {
-        int len = s.length();
-        // Track last position of a, b, c
-        int[] lastPos = { -1, -1, -1 };
-        int total = 0;
-
-        for (int pos = 0; pos < len; pos++) {
-            // Update last position of current character
-            lastPos[s.charAt(pos) - 'a'] = pos;
-
-            // Add count of valid substrings ending at current position
-            // If any character is missing, min will be -1
-            // Else min gives leftmost required character position
-            total += 1 + Math.min(lastPos[0], Math.min(lastPos[1], lastPos[2]));
+        int lastSeen[] = {-1,-1,-1};
+        int cnt = 0;
+        for(int i=0;i<s.length();i++){
+            lastSeen[s.charAt(i)-'a'] = i;
+            if(lastSeen[0]!=-1 && lastSeen[1]!=-1 && lastSeen[2]!=-1){
+                cnt = cnt+(1+Math.min(lastSeen[0],Math.min(lastSeen[1],lastSeen[2])));
+            }
         }
-
-        return total;
+        return cnt;
     }
 }
