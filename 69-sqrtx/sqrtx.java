1511b7a9c386a7ba9cc3ee04fdd0e576fc5214fb
Prisha Mehta <143733402+prishamehta01@users.noreply.github.com>
2025-08-31 17:34:30 +0530

    Time: 1 ms (84.28%) | Memory: 41.2 MB (20.43%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -3,21 +3,18 @@ class Solution {
         if(x<2){
             return x;
         }
-        int left = 1;
-        int right = x/2;
-        int ans = 0;
-        while(left<=right){
-            int mid = left+(right-left)/2;
+        int beg = 0,end=x/2,ans=0;
+        while(beg<=end){
+            int mid = beg + (end-beg)/2;
             long sq = (long)mid*mid;
-            if(sq == x){
+            if(sq==x){
                 return mid;
-            }
-            else if(sq < x){
+            }else if(sq<x){
                 ans = mid;
-                left = mid+1;
+                beg = mid+1;
             }
             else{
-                right = mid-1;
+                end = mid-1;
             }
         }
         return ans;
