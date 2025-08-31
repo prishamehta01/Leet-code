5da26b2bf16f1aaae89c666cadaede128b4cda2a
Prisha Mehta <143733402+prishamehta01@users.noreply.github.com>
2025-08-31 17:36:19 +0530

    Time: 1 ms (84.28%) | Memory: 41.2 MB (20.43%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -3,20 +3,19 @@ class Solution {
         if(x<2){
             return x;
         }
-        int beg = 0,end=x/2,ans=0;
+        int beg = 0,end=x/2;
         while(beg<=end){
             int mid = beg + (end-beg)/2;
             long sq = (long)mid*mid;
             if(sq==x){
                 return mid;
             }else if(sq<x){
-                ans = mid;
                 beg = mid+1;
             }
             else{
                 end = mid-1;
             }
         }
-        return ans;
+        return end;
     }
 }
