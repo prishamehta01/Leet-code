cce10480f9a97f3c837b46fc89853233e50cb90d
Prisha Mehta <143733402+prishamehta01@users.noreply.github.com>
2025-09-07 20:32:56 +0530

    Time: 22 ms (28.51%) | Memory: 60.2 MB (5.14%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -1,38 +1,35 @@
 class Solution {
-    public boolean possible(int[] bloomDay,int m,int k,int day){
-        int cnt = 0;
-        int numBoquets = 0;
-        for(int i=0;i<bloomDay.length;i++){
-            if(bloomDay[i]<=day){
+    private boolean possible(int[] bloomDay, int m, int k, int day) {
+        int cnt = 0, bouquets = 0;
+        for (int d : bloomDay) {
+            if (d <= day) {
                 cnt++;
-            }
-            else{
-                numBoquets += (cnt/k);
+                if (cnt == k) {
+                    bouquets++;
+                    cnt = 0;
+                }
+            } else {
                 cnt = 0;
             }
+            if (bouquets >= m) return true; // early stop
         }
-        numBoquets += (cnt/k);
-        return numBoquets>=m?true:false;
+        return false;
     }
+
     public int minDays(int[] bloomDay, int m, int k) {
-        if ((long) m * k > bloomDay.length) {
-            return -1;
-        }
-        int low = 1,high=Integer.MAX_VALUE;
-        // for(int i=1;i<bloomDay.length;i++){
-        //     if(bloomDay[i]<low) low = bloomDay[i];
-        //     else if(bloomDay[i]>high) high = bloomDay[i];
-        // }
-        
-        int ans=-1;
-        while(low<=high){
-            int mid = low+(high-low)/2;
-            if(possible(bloomDay,m,k,mid)){
+        if ((long) m * k > bloomDay.length) return -1; // impossible case
+
+        int low = Arrays.stream(bloomDay).min().getAsInt();
+        int high = Arrays.stream(bloomDay).max().getAsInt();
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (possible(bloomDay, m, k, mid)) {
                 ans = mid;
-                high = mid-1;
-            }
-            else{
-                low = mid+1;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
         return ans;
