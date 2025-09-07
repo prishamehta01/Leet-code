ba72eb685ec3da1188a5f29f75173527bce08e85
Prisha Mehta <143733402+prishamehta01@users.noreply.github.com>
2025-09-07 20:30:57 +0530

    Time: 23 ms (17.00%) | Memory: 55.8 MB (96.52%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -18,15 +18,15 @@ class Solution {
         if ((long) m * k > bloomDay.length) {
             return -1;
         }
-        int low = bloomDay[0],high=bloomDay[0];
-        for(int i=1;i<bloomDay.length;i++){
-            if(bloomDay[i]<low) low = bloomDay[i];
-            else if(bloomDay[i]>high) high = bloomDay[i];
-        }
+        int low = 1,high=Integer.MAX_VALUE;
+        // for(int i=1;i<bloomDay.length;i++){
+        //     if(bloomDay[i]<low) low = bloomDay[i];
+        //     else if(bloomDay[i]>high) high = bloomDay[i];
+        // }
         
         int ans=-1;
         while(low<=high){
-            int mid = (low+high)/2;
+            int mid = low+(high-low)/2;
             if(possible(bloomDay,m,k,mid)){
                 ans = mid;
                 high = mid-1;
