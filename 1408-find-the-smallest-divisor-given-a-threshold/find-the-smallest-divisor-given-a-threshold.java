19153546c9f564d9d4ffb6aa400d17f3b8e308ed
Prisha Mehta <143733402+prishamehta01@users.noreply.github.com>
2025-09-08 15:24:51 +0530

    Time: 30 ms (33.80%) | Memory: 48.5 MB (26.53%) - LeetSync

--- 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.java
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.java
@@ -1,48 +1,27 @@
 class Solution {
-    public int smallestDivisor(int[] nums, int threshold) {
-        // in this we have to find We want the smallest divisor such that the sum of (ceil(nums[i]/divisor)) is ≤ threshold
-        // then we have to create a sum function which calculate the sum is grater or less than the threshold
-        // then we have to find out the low and high and apply binary search 
-
-        
-
-        int n = nums.length;
-
-        // edge case 
-        if(n > threshold) return -1;
-
-        
-       int max = Integer.MIN_VALUE;
-        for(int i = 0; i < n ; i++){
-        max = Math.max(nums[i] , max);
+    public boolean isDivisible(int[] nums,int threshold,int num){
+        int sum=0;
+        for(int n:nums){
+            sum+=Math.ceil(n/(double)num);
         }
-
-        int low = 1 , high = max;
-
-        while(low <= high){
-
-            int mid = (low + high ) / 2;
-
-            if(sum(nums,mid) <= threshold){
-                high = mid - 1;
-            }else{
-                low = mid + 1;
-            }
-
-        }
-
-        return low;
+        return sum<=threshold;
     }
-
-    public static int sum(int [] nums, int div){
-
-        int n = nums.length;
-         int sum = 0;
-        for(int i = 0; i < n ;i++){
-         
-          sum += Math.ceil((double)(nums[i])/(double)div);
-            
+    public int smallestDivisor(int[] nums, int threshold) {
+        int max = nums[0];
+        for(int n:nums){
+            if(n>max) max = n;
         }
-        return sum;
+        int low=1,high=max,ans=-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(isDivisible(nums,threshold,mid)){
+                ans = mid;
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        return ans;
     }
 }
