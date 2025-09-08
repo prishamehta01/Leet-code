1b680dd65ea60a1f37e2b55b347b726dc29ce7c2
Prisha Mehta <143733402+prishamehta01@users.noreply.github.com>
2025-09-08 15:24:39 +0530

    Time: 30 ms (33.80%) | Memory: 48 MB (91.56%) - LeetSync

--- 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.java
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.java
@@ -1,27 +1,48 @@
 class Solution {
-    public boolean isDivisible(int[] nums,int threshold,int num){
-        int sum=0;
-        for(int n:nums){
-            sum+=Math.ceil(n/(double)num);
-        }
-        return sum<=threshold;
-    }
     public int smallestDivisor(int[] nums, int threshold) {
-        int max = nums[0];
-        for(int n:nums){
-            if(n>max) max = n;
+        // in this we have to find We want the smallest divisor such that the sum of (ceil(nums[i]/divisor)) is ≤ threshold
+        // then we have to create a sum function which calculate the sum is grater or less than the threshold
+        // then we have to find out the low and high and apply binary search 
+
+        
+
+        int n = nums.length;
+
+        // edge case 
+        if(n > threshold) return -1;
+
+        
+       int max = Integer.MIN_VALUE;
+        for(int i = 0; i < n ; i++){
+        max = Math.max(nums[i] , max);
         }
-        int low=1,high=max,ans=-1;
-        while(low<=high){
-            int mid = (low+high)/2;
-            if(isDivisible(nums,threshold,mid)){
-                ans = mid;
-                high = mid-1;
+
+        int low = 1 , high = max;
+
+        while(low <= high){
+
+            int mid = (low + high ) / 2;
+
+            if(sum(nums,mid) <= threshold){
+                high = mid - 1;
+            }else{
+                low = mid + 1;
             }
-            else{
-                low = mid+1;
-            }
+
         }
-        return ans;
+
+        return low;
+    }
+
+    public static int sum(int [] nums, int div){
+
+        int n = nums.length;
+         int sum = 0;
+        for(int i = 0; i < n ;i++){
+         
+          sum += Math.ceil((double)(nums[i])/(double)div);
+            
+        }
+        return sum;
     }
 }
