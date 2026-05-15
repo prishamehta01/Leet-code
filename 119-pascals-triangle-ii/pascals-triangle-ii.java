bf9a91f26c3b5ac0789043efd1bafa529a349799
Prisha Mehta <143733402+prishamehta01@users.noreply.github.com>
2026-05-15 23:36:26 +0530

    Time: 1 ms (69.65%) | Memory: 42.8 MB (6.42%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.java
+++ 119-pascals-triangle-ii/pascals-triangle-ii.java
@@ -1,6 +1,6 @@
 class Solution {
     public List<Integer> getRow(int rowIndex) {
-        ArrayList<Integer> row = new ArrayList<>();
+        List<Integer> row = new ArrayList<>();
         for(int i=0;i<=rowIndex;i++){
             row.add(1);
             for(int j=i-1;j>0;j--){
