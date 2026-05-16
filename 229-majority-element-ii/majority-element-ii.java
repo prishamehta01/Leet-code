4d42ddd32a04d8cad3ca6bac39b0c984e4c1243e
Prisha Mehta <143733402+prishamehta01@users.noreply.github.com>
2026-05-17 00:08:07 +0530

    Time: 2 ms (99.91%) | Memory: 52.5 MB (51.85%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,11 +1,10 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int n = nums.length;
-        int c1=0,c2=1;
-        int cnt1=0,cnt2 = 0;
+        int c1=0,cnt1=0,c2=0,cnt2=0;
+        ArrayList<Integer> res = new ArrayList<>();
         for(int num:nums){
-            if(num==c1) cnt1++;
-            else if(num==c2) cnt2++;
+            if(c1==num) cnt1++;
+            else if(c2==num) cnt2++;
             else if(cnt1==0){
                 c1 = num;
                 cnt1 = 1;
@@ -14,19 +13,18 @@ class Solution {
                 c2 = num;
                 cnt2 = 1;
             }
-            else{
+            else {
                 cnt1--;
                 cnt2--;
             }
+        } 
+        cnt1=0;cnt2=0;
+        for(int n:nums){
+            if(n==c1) cnt1++;
+            else if(n==c2) cnt2++;
         }
-        cnt1=cnt2=0;
-        List<Integer> res = new ArrayList<>();
-        for(int num:nums){
-            if(num==c1) cnt1++;
-            else if(num==c2) cnt2++;
-        }
-        if(cnt1 > n/3) res.add(c1);
-        if(cnt2 > n/3) res.add(c2);
+        if(cnt1>nums.length/3) res.add(c1);
+        if(cnt2>nums.length/3) res.add(c2);
         return res;
     }
 }
