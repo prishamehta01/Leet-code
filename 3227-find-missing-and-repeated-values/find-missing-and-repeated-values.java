05a4c5b70e310efc12993863e2e9e18b09d61d53
Prisha Mehta <143733402+prishamehta01@users.noreply.github.com>
2026-06-06 15:10:48 +0530

    Time: 1 ms (100.00%) | Memory: 47.2 MB (75.03%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
@@ -1,36 +1,22 @@
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
-
         int n = grid.length;
-        long N = (long) n * n;
-
-        long expectedSum =
-                N * (N + 1) / 2;
-
-        long expectedSqSum =
-                N * (N + 1) * (2 * N + 1) / 6;
-
+        long N = (long)(n*n);
+        long expectedSum = (N*(N+1))/2;
+        long expectedSq = (N*(N+1)*((2*N)+1))/6;
         long actualSum = 0;
-        long actualSqSum = 0;
-
-        for (int[] row : grid) {
-            for (int val : row) {
-                actualSum += val;
-                actualSqSum += (long) val * val;
+        long actualSq = 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                actualSum+=grid[i][j];
+                actualSq+=(long)(grid[i][j])*grid[i][j];
             }
         }
-
         long D = actualSum - expectedSum;
-        long E = actualSqSum - expectedSqSum;
-
-        long sumXY = E / D;
-
-        int repeat =
-                (int) ((D + sumXY) / 2);
-
-        int miss =
-                (int) (sumXY - repeat);
-
-        return new int[]{repeat, miss};
+        long E = actualSq - expectedSq;
+        long sumXY = E/D;
+        int repeated = (int)((sumXY+D)/2);
+        int missing = (int)(sumXY-repeated);
+        return new int[]{repeated,missing};
     }
 }
