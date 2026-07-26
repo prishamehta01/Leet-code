c61065dc0bf16dd9694d73cfd733be9af61ccac1
Prisha Mehta <143733402+prishamehta01@users.noreply.github.com>
2026-07-26 22:07:19 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (38.59%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -6,7 +6,12 @@ class Solution:
             return 0
         if nums[len(nums)-1]>nums[len(nums)-2]:
             return len(nums)-1
-        for i in range(1,len(nums)-1):
-            if nums[i]>nums[i-1] and nums[i]>nums[i+1]:
-                return i
-        
+        low,high = 0,len(nums)-2
+        while low<=high:
+            mid = low+(high-low)//2
+            if nums[mid]>nums[mid-1] and nums[mid]>nums[mid+1]:
+                return mid
+            elif nums[mid]<nums[mid+1]:
+                low=mid+1
+            else:
+                high = mid-1
