f8e706f1595ba3b8ef9f7fecd278889bfeaf6d23
Prisha Mehta <143733402+prishamehta01@users.noreply.github.com>
2026-08-29 19:47:40 +0530

    Time: 1 ms (100.00%) | Memory: 48.7 MB (71.39%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,21 +1,34 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        //Ensure nums1 is the smaller array, to reduce search space of binary search
+        if(nums2.length<nums1.length){
+            return findMedianSortedArrays(nums2,nums1);
+        }
         int n1 = nums1.length;
         int n2 = nums2.length;
-        int medianIndex =  (n1+n2)/2;
-        int i=0,j=0;
-        int cur=0,prev=0;
-        for(int cnt=0;cnt<=medianIndex;cnt++){
-            prev = cur;
-            if(i<n1 && (j>=n2 ||nums1[i]<nums2[j])){
-                cur = nums1[i++];
+        int low=0,high=n1;
+        while(low<=high){
+            int cut1 = (low+high)/2;
+            int cut2 = (n1+n2+1)/2 - cut1; //ensuring the no of ele to be picked to complete left half -- if len of n1 and n2 = 10, then half = 5, so cut 1 = 2, cut2 = 3(5-2)
+            int l1 = (cut1==0)?Integer.MIN_VALUE:nums1[cut1-1];
+            int l2 = (cut2==0)?Integer.MIN_VALUE:nums2[cut2-1];
+            int r1 = (cut1==n1)?Integer.MAX_VALUE:nums1[cut1];
+            int r2 = (cut2==n2)?Integer.MAX_VALUE:nums2[cut2];
+            if(l1<=r2 && l2<=r1){
+                if((n1+n2)%2==0){
+                    return (Math.max(l1,l2)+Math.min(r1,r2))/2.0;
+                }
+                else{
+                    return Math.max(l1,l2);
+                }
+            }
+            else if(l1>r2){
+                high = cut1-1;
             }
             else{
-                cur = nums2[j++];
+                low = cut1+1;
             }
-
         }
-        double res = (n1+n2)%2==0?((cur+prev)/2.0):cur;
-        return res;
+        return 0.0;
     }
 }
